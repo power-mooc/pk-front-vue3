9ea4461f2418f4e12bc07608158e6339f62895c0
power <1410837981@qq.com>
2024-05-08 23:28:18 +0800

    fix：解决导出vuets问题

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
 
+console.log(import.meta.env.BASE_URL);
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -18,6 +19,6 @@ const router = createRouter({
       component: () => import('../views/AboutView.vue')
     }
   ]
-})
+});
 
-export default router
+export default router;
