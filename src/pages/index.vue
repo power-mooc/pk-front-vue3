023c9e1c046fd7cc1d2dda0bed3864a36d696fa5
power <1410837981@qq.com>
2024-05-13 23:34:45 +0800

    feat: add vite-plugin-mock

---
+++ src/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1>This is index page</h1>
+  </div>
+</template>
+<style></style>
+
+<route lang="yaml">
+meta:
+  layout: default
+</route>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import VueDevTools from 'vite-plugin-vue-devtools';
@@ -14,52 +14,59 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'; // elem
 import Icons from 'unplugin-icons/vite'; // iconfont图标
 import IconsResolver from 'unplugin-icons/resolver'; // iconfont
 import Layouts from 'vite-plugin-vue-layouts'; // layous布局
+import { viteMockServe } from 'vite-plugin-mock'; //mock数据
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    VueRouter({}),
-    vue(),
-    vueJsx(),
-    VueDevTools(),
-    UnoCSS(),
-    AutoImport({
-      include: [
-        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
-        /\.vue$/,
-        /\.vue\?vue/, // .vue
-        /\.md$/ // .md
-      ],
-      imports: ['vue', '@vueuse/core', VueRouterAutoImports],
-      resolvers: [
-        ElementPlusResolver(),
-        // 自动导入图标组件
-        IconsResolver({
-          prefix: 'Icon'
-        })
-      ]
-    }),
-    Components({
-      resolvers: [
-        ElementPlusResolver(),
-        // 自动注册图标组件
-        IconsResolver({
-          enabledCollections: ['ep']
-        })
-      ]
-    }),
-    Icons({
-      autoInstall: true
-    }),
-    Layouts({
-      layoutsDirs: 'src/layouts',
-      pagesDirs: 'src/pages',
-      defaultLayout: 'default'
-    })
-  ],
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      VueRouter({}),
+      vue(),
+      vueJsx(),
+      VueDevTools(),
+      UnoCSS(),
+      AutoImport({
+        include: [
+          /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+          /\.vue$/,
+          /\.vue\?vue/, // .vue
+          /\.md$/ // .md
+        ],
+        imports: ['vue', '@vueuse/core', VueRouterAutoImports],
+        resolvers: [
+          ElementPlusResolver(),
+          // 自动导入图标组件
+          IconsResolver({
+            prefix: 'Icon'
+          })
+        ]
+      }),
+      Components({
+        resolvers: [
+          ElementPlusResolver(),
+          // 自动注册图标组件
+          IconsResolver({
+            enabledCollections: ['ep']
+          })
+        ]
+      }),
+      Icons({
+        autoInstall: true
+      }),
+      Layouts({
+        layoutsDirs: 'src/layouts',
+        pagesDirs: 'src/pages',
+        defaultLayout: 'default'
+      }),
+      viteMockServe({
+        mockPath: 'mock',
+        enable: mode === 'development'
+      })
+    ],
 
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
-  }
+  };
 });
