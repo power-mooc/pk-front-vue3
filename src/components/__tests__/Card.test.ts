5dfeb5699b821458d28e8b3be1bc5f9523203275
power <1410837981@qq.com>
2024-07-04 21:55:25 +0800

    feat: add vitest

--- src/components/__tests__/Card.test.ts
+++ src/components/__tests__/Card.test.ts
@@ -1,6 +1,47 @@
-import { expect, it } from 'vitest';
-it('Card component unit test', () => {
-  // test logic
-  // test result
-  expect(true).toBe(true);
+import { describe, expect, it } from 'vitest';
+import { shallowMount } from '@vue/test-utils';
+
+import Card from '@/components/Card.vue';
+
+describe('Card.vue', () => {
+  it('renders card title and content titleClass', () => {
+    const title = 'Card Title';
+    const subTitle = 'Card Content';
+    const titleClass = 'p-4';
+    const wrapper = shallowMount(Card, {
+      propsData: {
+        title,
+        subTitle,
+        titleClass
+      }
+    });
+    expect(wrapper.text()).toMatch(title);
+    expect(wrapper.text()).toMatch(subTitle);
+
+    let flag = false;
+    wrapper.findAll('div').forEach((el) => {
+      if (el.classes().includes(titleClass) && el.classes().includes('lg:p-4')) {
+        flag = true;
+      }
+    });
+    expect(flag).toBe(true);
+  });
+
+  it('test card icon attrbute', () => {
+    const icon = 'i-mdi:web';
+    const wrapper = shallowMount(Card, {
+      propsData: { icon }
+    });
+    const html = wrapper.html();
+    expect(html).toMatch(icon);
+  });
+  // 反向测试icon
+  it('when prop image, it is not have icon', () => {
+    const icon = 'i-mdi:web';
+    const wrapper = shallowMount(Card, {
+      propsData: { image: 'test.com' }
+    });
+    const html = wrapper.html();
+    expect(html).not.toMatch(icon);
+  });
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,23 @@
 import { fileURLToPath } from 'node:url';
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config';
 import viteConfig from './vite.config';
-export default mergeConfig(
-  // fix ts
-  viteConfig({ mode: process.env.NODE_ENV || '', command: 'serve' }),
-  defineConfig({
-    test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'cypress/**', 'core/**'],
-      root: fileURLToPath(new URL('./', import.meta.url))
-    }
-  })
+
+export default defineConfig((configEnv) =>
+  mergeConfig(
+    viteConfig(configEnv),
+    defineConfig({
+      test: {
+        environment: 'jsdom',
+        exclude: [...configDefaults.exclude, 'cypress/**', 'core/**'],
+        root: fileURLToPath(new URL('./', import.meta.url)),
+        coverage: {
+          provider: 'v8',
+          reporter: ['text', 'json', 'html'],
+          include: ['src/components'],
+          // 覆盖率报告
+          enabled: true
+        }
+      }
+    })
+  )
 );
